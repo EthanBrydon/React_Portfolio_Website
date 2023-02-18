225207dfe6ab9d98ac21176b10628b1329b1f676
Ethan Brydon <59847782+EthanBrydon@users.noreply.github.com>
2023-02-18 14:43:54 -0500

    Updated CV & Photo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/random_dude.jpg'
+import ME from '../../assets/me_photo.jpg'
 import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
