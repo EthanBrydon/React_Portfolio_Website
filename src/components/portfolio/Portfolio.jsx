098dd6cb9ee46f4fb2ea68bad338e266bafa66b8
Ethan Brydon <59847782+EthanBrydon@users.noreply.github.com>
2023-03-18 13:46:14 -0400

    Completed services and started portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,19 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <h3>Portfolio Item Title</h3>
+            <a href=""></a>
+          </div>
+        </article>
+      </div> 
+    </section>
   )
 
 }
