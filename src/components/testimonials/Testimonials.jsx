8b27390b9dc3601312fdf4fbf3ab7f4439eccb75
Ethan Brydon <59847782+EthanBrydon@users.noreply.github.com>
2023-03-29 18:11:21 -0400

    Added Swiper Js and completed Testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,54 +5,68 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+// import Swiper core and required modules
+import { Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const testimonials_sections = [
+  {
+    avatar: AVTR1,
+    name: 'Average Joe',
+    review: '**Paragraph**'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Average Joe',
+    review: '**Paragraph**'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Average Joe',
+    review: '**Paragraph**'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Average Joe',
+    review: '**Paragraph**'
+  }
+]
+
+
 const Testimonials = () => {
   return (
     <section id='testimonials'>
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className='container testimonials__container'>
-        <article className='testimonial'>
-          <div className='clien__avatar'>
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5 className='client__name'>Average Joe</h5>
-          <small className='client__review'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt mollitia natus iste enim hic,  
-            nihil nesciunt quis dolorum ducimus corrupti recusandae 
-            vitae reiciendis delectus harum aliquid vero molestias pariatur! Nam?
-          </small>
-        </article>
-        <article className='testimonial'>
-          <div className='clien__avatar'>
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5 className='client__name'>Average Joe</h5>
-          <small className='client__review'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt mollitia natus iste enim hic,  
-            nihil nesciunt quis dolorum ducimus corrupti recusandae 
-            vitae reiciendis delectus harum aliquid vero molestias pariatur! Nam?
-          </small>
-        </article>
-        <article className='testimonial'>
-          <div className='clien__avatar'>
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5 className='client__name'>Average Joe</h5>
-          <small className='client__review'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt mollitia natus iste enim hic,  
-            nihil nesciunt quis dolorum ducimus corrupti recusandae 
-            vitae reiciendis delectus harum aliquid vero molestias pariatur! Nam?
-          </small>
-        </article>
-        <article className='testimonial'>
-          <div className='clien__avatar'>
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5 className='client__name'>Average Joe</h5>
-          <small className='client__review'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt mollitia natus iste enim hic,  
-            nihil nesciunt quis dolorum ducimus corrupti recusandae 
-            vitae reiciendis delectus harum aliquid vero molestias pariatur! Nam?
-          </small>
-        </article> 
-      </div>
+      <Swiper className='container testimonials__container'
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}>
+        {
+          testimonials_sections.map(({avatar, name, review}, index) => {
+            return (
+              <SwiperSlide key={index} className='testimonial'>
+                <div className='client__avatar'>
+                  <img src={avatar} alt='Avatar'/>
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'> 
+                  {review}
+                </small>
+              </SwiperSlide> 
+            )
+          })
+        }
+      </Swiper>
     </section>
   )
 }
